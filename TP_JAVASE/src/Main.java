62474bc6b437b3efe5b671931be7150464d8f15c
kkwaxy <kkwaxy@gmail.com>
2020-05-31 12:01:13 +0200

    Ajout de la methode static nombreInstance

--- TP_JAVASE/src/Main.java
+++ TP_JAVASE/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String args[]){
         Point P1 = new Point(50,2);
         P1.afficher();
+        System.out.println(Point.nombreInstance());
     }    
 }
--- TP_JAVASE/src/com/inphb/atelier1/Point.java
+++ TP_JAVASE/src/com/inphb/atelier1/Point.java
@@ -5,12 +5,21 @@ package com.inphb.atelier1;
  * @author kkwaxy
  */
 public class Point {
-    int x,y;
     
-   Point(){} 
+   private int x,y;
    
-   public Point(int x, int y){this.x = x;this.y = y;}
-  
+   private static int compte=0; 
+   
+   Point(){compte+=1;} 
+   
+   public Point(int x, int y){this.x = x;this.y = y;compte+=1;};
+   
+   //public Point(Point point){this.x=point.x;this.y=point.y;compte+=1;};
+   
+   public Point(Point point){this(point.x,point.y);compte+=1;};
+   
+   public static int nombreInstance(){return(compte);};
+   
    public int abscisse(){return(this.x);}
   
    public int ordonnee(){return(this.y);}
